c8c996e775923421261be208c2ff49507525f0c2
Hasib <130287429+Haaasib@users.noreply.github.com>
2024-08-17 20:33:35 +0600

    Update cfg.lua

--- cfg.lua
+++ cfg.lua
@@ -38,91 +38,91 @@ Config.Mosleys = {
         },
          [4] = {
              ped = 'a_m_m_eastsa_01', 
-             vehicleModel = 'zentorno',  - 
+             vehicleModel = 'zentorno',  
              vehicleCoords = vector3(975.73, -2250.03, 30.56), 
              pedCoords = vector4(976.18, -2254.50, 29.54, 352.73), 
          },
          [5] = {
             ped = 'a_m_m_hillbilly_02', 
-            vehicleModel = 'previon',  - 
+            vehicleModel = 'previon',  
             vehicleCoords = vector3(786.36, -2529.84, 20.73), 
             pedCoords = vector4(794.51, -2529.46, 20.41, 92.44), 
         },
         [6] = {
             ped = 'a_m_m_rurmeth_01', 
-            vehicleModel = 'dominator2',  - 
+            vehicleModel = 'dominator2',  
             vehicleCoords = vector3(786.36, -2529.84, 20.73), 
             pedCoords = vector4(794.51, -2529.46, 20.41, 92.44), 
         },
         [7] = {
             ped = 'a_m_m_salton_01', 
-            vehicleModel = 'hotknife',  - 
+            vehicleModel = 'hotknife',   
             vehicleCoords = vector3(547.50, -1926.36, 24.81), 
             pedCoords = vector4(543.01, -1929.20, 23.99, 299.77), 
         },
         [8] = {
             ped = 'a_m_m_salton_02', 
-            vehicleModel = 'hermes',  - 
+            vehicleModel = 'hermes',  
             vehicleCoords = vector3(52.55, -1577.96, 29.46), 
             pedCoords = vector4(55.31, -1580.36, 28.60, 57.51), 
         },
         [9] = {
             ped = 'a_m_m_salton_03', 
-            vehicleModel = 'ruiner',  - 
+            vehicleModel = 'ruiner',   
             vehicleCoords = vector3(-188.97, -1290.60, 31.30), 
             pedCoords = vector4(-193.70, -1290.65, 30.30, 266.98), 
         },
         [10] = {
             ped = 'a_m_m_salton_04', 
-            vehicleModel = 'yosemite2',  - 
+            vehicleModel = 'yosemite2',   
             vehicleCoords = vector3(-310.75, -1028.20, 30.39), 
             pedCoords = vector4(-307.12, -1029.69, 29.38, 71.29), 
         },
         [11] = {
             ped = 'a_m_m_trampbeac_01', 
-            vehicleModel = 'brutus3',  - 
+            vehicleModel = 'brutus3',   
             vehicleCoords = vector3(305.62, -904.97, 29.29), 
             pedCoords = vector4(309.45, -906.34, 28.29, 92.54), 
         },
         [12] = {
             ped = 'a_m_m_tranvest_01', 
-            vehicleModel = 'trophytruck2',  - 
+            vehicleModel = 'trophytruck2',   
             vehicleCoords = vector3(81.14, 20.84, 69.09), 
             pedCoords = vector4(75.70, 20.09, 68.16, 179.18), 
         },
         [13] = {
             ped = 'a_m_o_tramp_01', 
-            vehicleModel = 'dubsta2',  - 
+            vehicleModel = 'dubsta2',   
             vehicleCoords = vector3(-277.33, 309.19, 93.40), 
             pedCoords = vector4(-277.68, 311.68, 97.25, 1.03), 
         },
         [14] = {
             ped = 'a_m_y_beach_01', 
-            vehicleModel = 'bestiagts',  - 
+            vehicleModel = 'bestiagts',  
             vehicleCoords = vector3(-2163.69, -380.25, 13.17), 
             pedCoords = vector4(-2162.92, -377.06, 12.28, 160.59), 
         },
         [15] = {
             ped = 'a_m_y_beach_02', 
-            vehicleModel = 'raiden',  - 
+            vehicleModel = 'raiden',   
             vehicleCoords = vector3(856.38, 2192.70, 52.23), 
             pedCoords = vector4(856.66, 2188.30, 51.29, 330.10), 
         },
         [16] = {
             ped = 'a_m_y_beachvesp_01', 
-            vehicleModel = 'bjxl',  - 
+            vehicleModel = 'bjxl',   
             vehicleCoords = vector3(182.29, 2793.30, 45.66), 
             pedCoords = vector4(180.05, 2793.03, 44.66, 276.05), 
         },
         [17] = {
             ped = 'a_m_y_downtown_01', 
-            vehicleModel = 'landstalker',  - 
+            vehicleModel = 'landstalker',   
             vehicleCoords = vector3(962.88, 3610.76, 32.80), 
             pedCoords = vector4(963.41, 3613.72, 31.72, 182.51), 
         },
         [18] = {
             ped = 'a_m_y_indian_01', 
-            vehicleModel = 'flashgt',  - 
+            vehicleModel = 'flashgt',   
             vehicleCoords = vector3(2660.95, 3274.51, 55.24), 
             pedCoords = vector4(2663.47, 3273.11, 54.24, 62.23), 
         },
