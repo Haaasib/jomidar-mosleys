6aa2a9627c2f2bc46f10dd3fffc3b3e6a1ed22dc
Hasib <130287429+Haaasib@users.noreply.github.com>
2024-08-26 02:27:06 +0600

    Update cl.lua

--- cl.lua
+++ cl.lua
@@ -86,7 +86,7 @@ function driver1(vehicleNetId, interactionId)
         flags = 49,
      }, {}, {}, function()  -- Success
         if QBCore.Functions.HasItem('mosleyssideglass') then
-        TriggerServerEvent('jomidar:mosleys:removeItem', 'mosleyssideglass')
+        TriggerServerEvent('jomidar:mosleys:removeItem', 'mosleyssideglass', Config.Itemammountneed)
         QBCore.Functions.Notify("Sucessfully installed part to vehicle", "success")
         exports['jomidar-ui']:Show('Mosleys', 'Install Part 1/3')
         exports.interact:RemoveEntityInteraction(vehicleNetId, interactionId)
@@ -112,7 +112,7 @@ function driver2(vehicleNetId, interactionId)
         flags = 49,
      }, {}, {}, function()  -- Success
         if QBCore.Functions.HasItem('mosleysliv') then
-        TriggerServerEvent('jomidar:mosleys:removeItem', 'mosleysliv')
+        TriggerServerEvent('jomidar:mosleys:removeItem', 'mosleysliv', Config.Itemammountneed)
         QBCore.Functions.Notify("Sucessfully installed part to vehicle", "success")
         exports['jomidar-ui']:Show('Mosleys', 'Install Part 2/3')
         exports.interact:RemoveEntityInteraction(vehicleNetId, interactionId)
@@ -139,7 +139,7 @@ function engine(vehicleNetId, interactionId)
      }, {}, {}, function()  -- Success
         if QBCore.Functions.HasItem('mosleyshorn') then
 
-        TriggerServerEvent('jomidar:mosleys:removeItem', 'mosleyshorn')
+        TriggerServerEvent('jomidar:mosleys:removeItem', 'mosleyshorn' , Config.Itemammountneed)
         QBCore.Functions.Notify("Sucessfully installed part to vehicle", "success")
         exports['jomidar-ui']:Show('Mosleys', 'Install Part 3/3')
         exports.interact:RemoveEntityInteraction(vehicleNetId, interactionId)
@@ -187,7 +187,7 @@ function ZoneHandler()
 end
 
 Citizen.CreateThread(function()
-
+   
     local pedData = Config.Mosleys.StartPed[1]
     local pedModel1 = GetHashKey(pedData.ped)
 
@@ -216,6 +216,237 @@ Citizen.CreateThread(function()
                     TriggerEvent("jomidar-mosleys:cl:start")
                 end,
             }
+        }
+    })
+
+    exports.interact:AddInteraction({
+        coords = vector3(2332.82, 3043.02, 48.15),
+        distance = 4.0, -- optional
+        interactDst = 2.0, -- optional
+        id = 'ac', -- needed for removing interactions
+        name = 'chopac', -- optional
+        options = {
+             {
+                label = 'Chop Broken AC',
+                action = function(entity, coords, args)
+                    local hasItem = QBCore.Functions.HasItem('ac_broken', 2)
+                    if hasItem then
+                        local propCoords = vector4(2332.76, 3043.02, 47.15, 150)
+                        local propModel = 'prop_aircon_s_04a'
+                    
+                        RequestModel(propModel)
+                        while not HasModelLoaded(propModel) do
+                            Wait(0)
+                        end
+                    
+                        local prop = CreateObject(propModel, propCoords.x, propCoords.y, propCoords.z, true, true, false)
+                        SetEntityHeading(prop, propCoords.w)
+                        FreezeEntityPosition(prop, true)
+                    
+                        -- Load welding torch model
+                        local torchModel = `prop_weld_torch`
+                        RequestModel(torchModel)
+                        while not HasModelLoaded(torchModel) do
+                            Wait(0)
+                        end
+                    
+                        -- Create welding torch prop and attach it to the player's hand
+                        local playerPed = PlayerPedId()
+                        local weldingTorch = CreateObject(torchModel, 0.0, 0.0, 0.0, true, true, true)
+                        AttachEntityToEntity(weldingTorch, playerPed, GetPedBoneIndex(playerPed, 57005), 0.1, 0.0, 0.0, 0.0, 90.0, 0.0, true, true, false, true, 1, true)
+                    
+                        -- Start progress bar with welding animation
+                        QBCore.Functions.Progressbar("acchop1", "Chopping The AC Parts.....", 5000, false, true, {
+                            disableMovement = true,
+                            disableCarMovement = false,
+                            disableMouse = false,
+                            disableCombat = true,
+                        }, {
+                            animDict = "amb@world_human_welding@male@base",
+                            anim = "base",
+                            flags = 49
+                        }, {}, {}, function() -- Success
+                            DeleteObject(prop)
+                            DeleteObject(weldingTorch) -- Delete the torch prop after the task is done
+                            TriggerServerEvent('jomidar:mosleys:removeItem', 'ac_broken', 2)
+                            TriggerServerEvent('jomidar:mosleys:addItem', 'mosleyssideglass', 1)
+
+                        end, function() -- Cancel
+                            DeleteObject(prop)
+                            DeleteObject(weldingTorch) -- Delete the torch prop if the task is canceled
+                            QBCore.Functions.Notify("You Stopped Chopping AC", "error")
+                        end)
+                    else
+                        QBCore.Functions.Notify("You don't have enough items on you", "error")
+                    end
+                    
+                end,
+            },
+            {
+                label = 'Chop AC Compressor',
+                action = function(entity, coords, args)
+                    local hasItem = QBCore.Functions.HasItem('ac_compressor', 2)
+                    if hasItem then
+                        local propCoords = vector4(2332.76, 3043.02, 47.15, 150)
+                        local propModel = 'prop_aircon_s_04a'
+                    
+                        RequestModel(propModel)
+                        while not HasModelLoaded(propModel) do
+                            Wait(0)
+                        end
+                    
+                        local prop = CreateObject(propModel, propCoords.x, propCoords.y, propCoords.z, true, true, false)
+                        SetEntityHeading(prop, propCoords.w)
+                        FreezeEntityPosition(prop, true)
+                    
+                        -- Load welding torch model
+                        local torchModel = `prop_weld_torch`
+                        RequestModel(torchModel)
+                        while not HasModelLoaded(torchModel) do
+                            Wait(0)
+                        end
+                    
+                        -- Create welding torch prop and attach it to the player's hand
+                        local playerPed = PlayerPedId()
+                        local weldingTorch = CreateObject(torchModel, 0.0, 0.0, 0.0, true, true, true)
+                        AttachEntityToEntity(weldingTorch, playerPed, GetPedBoneIndex(playerPed, 57005), 0.1, 0.0, 0.0, 0.0, 90.0, 0.0, true, true, false, true, 1, true)
+                    
+                        -- Start progress bar with welding animation
+                        QBCore.Functions.Progressbar("acchop2", "Chopping The AC Parts.....", 5000, false, true, {
+                            disableMovement = true,
+                            disableCarMovement = false,
+                            disableMouse = false,
+                            disableCombat = true,
+                        }, {
+                            animDict = "amb@world_human_welding@male@base",
+                            anim = "base",
+                            flags = 49
+                        }, {}, {}, function() -- Success
+                            DeleteObject(prop)
+                            DeleteObject(weldingTorch) -- Delete the torch prop after the task is done
+                            TriggerServerEvent('jomidar:mosleys:removeItem', 'ac_compressor', 2)
+                            TriggerServerEvent('jomidar:mosleys:addItem', 'mosleyshorn', 1)
+                        end, function() -- Cancel
+                            DeleteObject(prop)
+                            DeleteObject(weldingTorch) -- Delete the torch prop if the task is canceled
+                            
+
+                            QBCore.Functions.Notify("You Stopped Chopping AC", "error")
+                        end)
+                    else
+                        QBCore.Functions.Notify("You don't have enough items on you", "error")
+                    end                end,
+            },
+            {
+                label = 'Chop AC',
+                action = function(entity, coords, args)
+                    local hasItem = QBCore.Functions.HasItem('ac', 2)
+                    if hasItem then
+                        local propCoords = vector4(2332.76, 3043.02, 47.15, 150)
+                        local propModel = 'prop_aircon_s_04a'
+                    
+                        RequestModel(propModel)
+                        while not HasModelLoaded(propModel) do
+                            Wait(0)
+                        end
+                    
+                        local prop = CreateObject(propModel, propCoords.x, propCoords.y, propCoords.z, true, true, false)
+                        SetEntityHeading(prop, propCoords.w)
+                        FreezeEntityPosition(prop, true)
+                    
+                        -- Load welding torch model
+                        local torchModel = `prop_weld_torch`
+                        RequestModel(torchModel)
+                        while not HasModelLoaded(torchModel) do
+                            Wait(0)
+                        end
+                    
+                        -- Create welding torch prop and attach it to the player's hand
+                        local playerPed = PlayerPedId()
+                        local weldingTorch = CreateObject(torchModel, 0.0, 0.0, 0.0, true, true, true)
+                        AttachEntityToEntity(weldingTorch, playerPed, GetPedBoneIndex(playerPed, 57005), 0.1, 0.0, 0.0, 0.0, 90.0, 0.0, true, true, false, true, 1, true)
+                    
+                        -- Start progress bar with welding animation
+                        QBCore.Functions.Progressbar("acchop3", "Chopping The AC Parts.....", 5000, false, true, {
+                            disableMovement = true,
+                            disableCarMovement = false,
+                            disableMouse = false,
+                            disableCombat = true,
+                        }, {
+                            animDict = "amb@world_human_welding@male@base",
+                            anim = "base",
+                            flags = 49
+                        }, {}, {}, function() -- Success
+                            DeleteObject(prop)
+                            DeleteObject(weldingTorch) -- Delete the torch prop after the task is done
+                            TriggerServerEvent('jomidar:mosleys:removeItem', 'ac', 2)
+                            TriggerServerEvent('jomidar:mosleys:addItem', 'mosleyshorn', 1)
+                        end, function() -- Cancel
+                            DeleteObject(prop)
+                            DeleteObject(weldingTorch) -- Delete the torch prop if the task is canceled
+                          
+
+                            QBCore.Functions.Notify("You Stopped Chopping AC", "error")
+                        end)
+                    else
+                        QBCore.Functions.Notify("You don't have enough items on you", "error")
+                    end                end,
+            },
+            {
+                label = 'Chop Ac Vent',
+                action = function(entity, coords, args)
+                    local hasItem = QBCore.Functions.HasItem('ac_vent', 2)
+                    if hasItem then
+                        local propCoords = vector4(2332.76, 3043.02, 47.15, 150)
+                        local propModel = 'prop_aircon_s_04a'
+                    
+                        RequestModel(propModel)
+                        while not HasModelLoaded(propModel) do
+                            Wait(0)
+                        end
+                    
+                        local prop = CreateObject(propModel, propCoords.x, propCoords.y, propCoords.z, true, true, false)
+                        SetEntityHeading(prop, propCoords.w)
+                        FreezeEntityPosition(prop, true)
+                    
+                        -- Load welding torch model
+                        local torchModel = `prop_weld_torch`
+                        RequestModel(torchModel)
+                        while not HasModelLoaded(torchModel) do
+                            Wait(0)
+                        end
+                    
+                        -- Create welding torch prop and attach it to the player's hand
+                        local playerPed = PlayerPedId()
+                        local weldingTorch = CreateObject(torchModel, 0.0, 0.0, 0.0, true, true, true)
+                        AttachEntityToEntity(weldingTorch, playerPed, GetPedBoneIndex(playerPed, 57005), 0.1, 0.0, 0.0, 0.0, 90.0, 0.0, true, true, false, true, 1, true)
+                    
+                        -- Start progress bar with welding animation
+                        QBCore.Functions.Progressbar("acchop4", "Chopping The AC Parts.....", 5000, false, true, {
+                            disableMovement = true,
+                            disableCarMovement = false,
+                            disableMouse = false,
+                            disableCombat = true,
+                        }, {
+                            animDict = "amb@world_human_welding@male@base",
+                            anim = "base",
+                            flags = 49
+                        }, {}, {}, function() -- Success
+                            DeleteObject(prop)
+                            DeleteObject(weldingTorch) -- Delete the torch prop after the task is done
+                            TriggerServerEvent('jomidar:mosleys:removeItem', 'ac_vent', 2)
+                            TriggerServerEvent('jomidar:mosleys:addItem', 'mosleysliv', 1)
+                        end, function() -- Cancel
+                            DeleteObject(prop)
+                            DeleteObject(weldingTorch) -- Delete the torch prop if the task is canceled
+                        
+                            QBCore.Functions.Notify("You Stopped Chopping AC", "error")
+                        end)
+                    else
+                        QBCore.Functions.Notify("You don't have enough items on you", "error")
+                    end                  
+                    end,
+            },
         }
     })
 
@@ -386,8 +617,9 @@ function SpawnVehicleForCustomer(data, PedBlip)
     exports['jomidar-ui']:Show('Mosleys', 'Go back to Mosleys')
     RemoveBlip(PedBlip)
 
-    -- Activate the marker when vehicle is spawned
+    -- Reset the flags
     markerActive = true
+    hasEnteredZone = false
 
     -- Start checking zone status
     Citizen.CreateThread(function()
@@ -404,11 +636,11 @@ end
 
 RegisterNetEvent('jomidar-mosleys:cl:start')
 AddEventHandler('jomidar-mosleys:cl:start', function()
-    local waittime = math.random(Config.MinTime, Config.MaxTime)
 
-    exports['jomidar-ui']:Show('Waiting for job offer')
-    Citizen.Wait(waittime)
-    exports['jomidar-ui']:Show('Mosleys', 'Go to the customer')
+        local waittime = math.random(Config.MinTime, Config.MaxTime)
+        exports['jomidar-ui']:Show('Waiting for job offer')
+        Citizen.Wait(waittime)
+        exports['jomidar-ui']:Show('Mosleys', 'Go to the customer')
+        SpawnMosleyPeds()
 
-    SpawnMosleyPeds()
 end)
